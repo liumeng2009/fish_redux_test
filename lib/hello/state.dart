a17cd056fd56d24af56d4ae45135cad34aad80f5
liumeng <378338627@qq.com>
2019-07-17 16:59:09 +0800

    201907171659
    201907171659

--- lib/hello/state.dart
+++ lib/hello/state.dart
@@ -1,4 +1,17 @@
+import 'dart:ui';
+
 import 'package:fish_redux/fish_redux.dart';
 
 import '../global_store/state.dart';
-class HelloState implements GlobalBaseState
+
+class HelloState implements GlobalBaseState, Cloneable<HelloState> {
+  @override
+  Color themeColor;
+
+  @override
+  HelloState clone() {
+    return HelloState()
+      ..themeColor = themeColor;
+  }
+
+}
